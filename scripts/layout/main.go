aa1d0663d1afc14f642c18b362aac8b25c429d64
suurkivi <136639517+suurkivi@users.noreply.github.com>
2023-09-20 15:51:52 -0700

    fix layout program

--- scripts/layout/main.go
+++ scripts/layout/main.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"math"
-	"os"
 )
 
 func Radians(degrees float64) float64 {
@@ -19,20 +17,29 @@ func cmToFeet(x float64) float64 {
 	return x / 12 / 2.54
 }
 
-var headPos = []int{4, 3, 1, 7, 8, 2, 5, 6, 9}
+var headPos = []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+//var tmpl = `
+//cameras: [ camera-%02d ]
+//heads: [ head-%02d ]
+//name: stand-%02d
+//pos: { x: %2f, y: %2f }
+//rot: -90
+//`
 
 var tmpl = `
-cameras: [ camera-%02d ]
-heads: [ head-%02d ]
-name: stand-%02d
-pos: { x: %2f, y: %2f }
-rot: -90
+[[Stands]]
+CameraNames = ['camera-%02d']
+HeadNames = ['head-%02d']
+Name = 'stand-%02d'
+Rot = -90.0
+Pos = { X = %2f, Y = %2f }
 `
 
 func main() {
 	const (
-		a            = 1323.34 / 100
-		b            = 121.92 / 100
+		a            = 1455.42 / 100
+		b            = 106.68 / 100
 		thetaDegrees = 5.28
 	)
 
@@ -48,10 +55,12 @@ func main() {
 		//fmt.Println(i, x, y, cmToFeet(x), cmToFeet(y))
 		stand := fmt.Sprintf(tmpl, pos, pos, pos, x, y)
 
-		outdir := os.ExpandEnv("$HOME/shared/theheads/scenes/hb2021/stands")
+		//outdir := os.ExpandEnv("$HOME/shared/theheads/scenes/hb2021/stands")
 
-		err := ioutil.WriteFile(outdir+fmt.Sprintf("/stand-%02d.yaml", pos), []byte(stand), 0o600)
-		noError(err)
+		//err := ioutil.WriteFile(outdir+fmt.Sprintf("/stand-%02d.yaml", pos), []byte(stand), 0o600)
+		//noError(err)
+
+		fmt.Println(stand)
 	}
 }
 
