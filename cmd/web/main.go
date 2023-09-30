03bedfbc8d31d2887d8b31f43d8a0826b0327b33
suurkivi <136639517+suurkivi@users.noreply.github.com>
2023-09-29 21:40:45 -0700

    fix web

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,9 +2,19 @@ package main
 
 import (
 	"github.com/minor-industries/platform/common/discovery"
+	"github.com/minor-industries/platform/common/metrics"
+	"github.com/minor-industries/platform/common/util"
 	"github.com/minor-industries/theheads/web"
 )
 
 func main() {
-	web.Run(discovery.NewSerf("127.0.0.1:7373"))
+	logger, _ := util.NewLogger(false)
+
+	if err := web.Run(
+		logger,
+		discovery.NewSerf("127.0.0.1:7373"),
+		metrics.NewRegistry(),
+	); err != nil {
+		panic(err)
+	}
 }
